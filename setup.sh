91a154a50636a0a23d32b4f62f55ff91a4592d21
zakk <115408369+SegusFaultise@users.noreply.github.com>
2026-05-22 02:08:22 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-#!/bin/bash
-
-# 1. Clone the repository (replace URL with your actual repo)
 git clone https://github.com/SegusFaultise/dotfiles.git ~/.dotfiles
 
-# 3. Create target directories
 mkdir -p ~/.config/{niri,waybar,alacritty,fuzzel}
 mkdir -p ~/.vim/{autoload,colors}
 
-# 4. Symlink Alacritty, Fuzzel, and Niri
 ln -sf ~/.dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf ~/.dotfiles/fuzzel/fuzzel.ini ~/.config/fuzzel/fuzzel.ini
 ln -sf ~/.dotfiles/niri/config.kdl ~/.config/niri/config.kdl
 
-# 5. Symlink Waybar files and the scripts directory
 ln -sf ~/.dotfiles/waybar/config.jsonc ~/.config/waybar/config.jsonc
 ln -sf ~/.dotfiles/waybar/style.css ~/.config/waybar/style.css
 ln -sfn ~/.dotfiles/waybar/scripts ~/.config/waybar/scripts
 
-# 6. Symlink Vim configurations
 ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/vim/autoload/plug.vim ~/.vim/autoload/plug.vim
 ln -sf ~/.dotfiles/vim/colors/naysayer.vim ~/.vim/colors/naysayer.vim
 
-# 7. Make Waybar scripts executable
 chmod +x ~/.dotfiles/waybar/scripts/*.sh
+
+vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
